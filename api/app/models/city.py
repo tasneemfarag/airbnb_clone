cd44035a07863f8f5a2febf8eb396e328fbe44a9
rickharris-dev <rickharris724@gmail.com>
2016-07-13 20:42:28 -0700

    Updated errors on files

--- api/app/models/city.py
+++ api/app/models/city.py
@@ -6,7 +6,8 @@ class City(BaseModel):
     state = ForeignKeyField(rel_model=State, related_name="cities", on_delete="CASCADE")
 
     def to_hash(self):
+        state = State.get(State.id == self.state)
         data = {}
         data['name'] = self.name
-        data['state_id'] = self.state
+        data['state_id'] = state.id
         return super(City, self).to_hash(self, data)
