50428af1532cea95798b101114c80d536735b679
rickharris-dev <rickharris724@gmail.com>
2016-08-09 09:03:42 -0700

    Updated amenity views and tests

--- api/app/views/amenity.py
+++ api/app/views/amenity.py
@@ -1,12 +1,15 @@
+''' Import app and models '''
+from app import app
 from app.models.amenity import Amenity
 from app.models.place import Place
 from app.models.place_amenity import PlaceAmenities
+
+''' Import packages '''
 from flask_json import as_json, request
 from flask import abort
-from app import app
 from datetime import datetime
-import json
 from peewee import OperationalError
+import json
 
 @app.route('/amenities', methods=['GET'])
 @as_json
@@ -24,8 +27,20 @@ def create_amenity():
     ''' Creates a new amenity '''
     data = request.get_json()
     try:
-        if data['name'] and not isinstance(data['name'], unicode):
-            raise OperationalError("Amenity 'name' must be a string value")
+        ''' Check if name for amenity was given '''
+        if not 'name' in data:
+            raise KeyError("'name'")
+
+        ''' Check if name is a string '''
+        if not isinstance(data['name'], unicode):
+            raise TypeError("Amenity 'name' must be a string value")
+
+        ''' Check if amenity already exists '''
+        query = Amenity.select().where(Amenity.name == data['name'])
+        if query.exists():
+            raise ValueError('Amenity already exists')
+
+        ''' Create new amenity '''
         new = Amenity.create(
             name = data['name']
         )
@@ -34,59 +49,84 @@ def create_amenity():
         res['id'] = new.id
         res['msg'] = "Amenity was created successfully"
         return res, 201
-    except KeyError as error:
-        response = {}
-        response['code'] = 400
-        response['msg'] = str(error.message) + ' is missing'
-        return response, 400
-    except Exception as error:
-        if type(error).__name__ == 'OperationalError':
-            response = {}
-            response['code'] = 400
-            response['msg'] = error.message
-            return response, 400
-        print error
-        print type(error)
-        print type(error).__name__
-        response = {}
-        response['code'] = 10003
-        response['msg'] = "Name already exists"
-        return response, 500
+    except KeyError as e:
+        res = {}
+        res['code'] = 400
+        res['msg'] = str(e.message) + ' is missing'
+        return res, 400
+    except TypeError as e:
+        res = {}
+        res['code'] = 400
+        res['msg'] = e.message
+        return res, 400
+    except ValueError as e:
+        res = {}
+        res['code'] = 10003
+        res['msg'] = e.message
+        return res, 409
+    except Exception as e:
+        res = {}
+        res['code'] = 500
+        res['msg'] = e.message
+        return res, 500
 
 @app.route('/amenities/<amenity_id>', methods=['GET'])
 @as_json
 def get_amenity(amenity_id):
     ''' Gets the details of the given amenity '''
     try:
+        ''' Check if amenity exists '''
+        query = Amenity.select().where(Amenity.id == amenity_id)
+        if not query.exists():
+            raise LookupError('amenity_id')
+
+        ''' Return amenity data '''
         amenity = Amenity.get(Amenity.id == amenity_id)
         return amenity.to_hash(), 200
-    except Exception as error:
-        if "Instance matching query does not exist" in error.message:
-            abort(404)
+    except LookupError as e:
+        abort(404)
+    except Exception as e:
+        res = {}
+        res['code'] = 500
+        res['msg'] = e.message
+        return res, 500
 
 @app.route('/amenities/<amenity_id>', methods=['DELETE'])
 @as_json
 def delete_amenity(amenity_id):
     ''' Deletes the given amenity '''
     try:
+        ''' Check if amenity exists '''
+        query = Amenity.select().where(Amenity.id == amenity_id)
+        if not query.exists():
+            raise LookupError('amenity_id')
+
+        ''' Delete the amenity '''
         amenity = Amenity.delete().where(Amenity.id == amenity_id)
         amenity.execute()
         res = {}
-        res['code'] = 201
+        res['code'] = 200
         res['msg'] = "Amenity was deleted successfully"
-        return res, 201
-    except Exception as error:
-        if "Instance matching query does not exist" in error.message:
-            abort(404)
+        return res, 200
+    except LookupError as e:
+        abort(404)
+    except Exception as e:
+        res = {}
+        res['code'] = 500
+        res['msg'] = e.message
+        return res, 500
 
 @app.route('/places/<place_id>/amenities', methods=['GET'])
 @as_json
 def get_place_amenities(place_id):
     ''' Gets all amenities for the given place '''
     try:
+        ''' Check if the place exists '''
         query = Place.select().where(Place.id == place_id)
         if not query.exists():
             raise LookupError('place_id')
+
+        ''' Return amenities for the given place '''
         amenities = []
         data = Amenity.select().join(PlaceAmenities).where(PlaceAmenities.place == place_id)
         for row in data:
@@ -94,9 +134,11 @@ def get_place_amenities(place_id):
         return {"result": amenities}, 200
     except LookupError as e:
         abort(404)
-    except Exception as error:
-        if "Instance matching query does not exist" in error.message:
-            abort(404)
+    except Exception as e:
+        res = {}
+        res['code'] = 500
+        res['msg'] = e.message
+        return res, 500
 
 @app.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'])
 @as_json
--- api/tests/amenity.py
+++ api/tests/amenity.py
@@ -32,97 +32,117 @@ class AppTestCase(unittest.TestCase):
         db.drop_tables([State, City, User, Place, Amenity, PlaceAmenities])
         db.close()
 
-    # def test_create(self):
-    #     ''' create correctly an amenity if all required parameters are send '''
-    #     rv = self.app.post('/amenities', headers={'Content-Type': 'application/json'}, data=json.dumps(good_amenity_1))
-    #     self.assertEqual(rv.status_code, 201)
-    #     data = json.loads(rv.data)
-    #     self.assertEqual(data['id'], 1)
-    #     rv = self.app.post('/amenities', headers={'Content-Type': 'application/json'}, data=json.dumps(good_amenity_2))
-    #     self.assertEqual(rv.status_code, 201)
-    #     data = json.loads(rv.data)
-    #     self.assertEqual(data['id'], 2)
-    #
-    #     ''' test all cases of missing parameters '''
-    #     rv = self.app.post('/amenities', headers={'Content-Type': 'application/json'}, data=json.dumps(bad_amenity_1))
-    #     self.assertEqual(rv.status_code, 400)
-    #     rv = self.app.post('/amenities', headers={'Content-Type': 'application/json'}, data=json.dumps(bad_amenity_2))
-    #     self.assertEqual(rv.status_code, 400)
-    #     rv = self.app.post('/amenities', headers={'Content-Type': 'application/json'}, data=json.dumps(bad_amenity_3))
-    #     self.assertEqual(rv.status_code, 400)
-    #
-    # def test_list(self):
-    #     ''' return 0 elements if no amenity was created'''
-    #     rv = self.app.get('/amenities')
-    #     self.assertEqual(rv.status_code, 200)
-    #     data = json.loads(rv.data)['result']
-    #     self.assertEqual(len(data), 0)
-    #
-    #     ''' return 1 element after an amenity creation'''
-    #     rv = self.app.post('/amenities', headers={'Content-Type': 'application/json'}, data=json.dumps(good_amenity_1))
-    #     rv = self.app.get('/amenities')
-    #     data = json.loads(rv.data)['result']
-    #     self.assertEqual(len(data), 1)
-    #
-    # def test_get(self):
-    #     ''' create an amenity and after get it '''
-    #     rv = self.app.post('/amenities', headers={'Content-Type': 'application/json'}, data=json.dumps(good_amenity_1))
-    #     rv = self.app.get('/amenities/1')
-    #
-    #     ''' check the status code '''
-    #     self.assertEqual(rv.status_code, 200)
-    #
-    #     ''' check if it's the same resource as during the creation '''
-    #     amenity = json.loads(rv.data)
-    #     self.assertEqual(amenity['name'], good_amenity_1['name'])
-    #
-    #     ''' check when trying to get an unknown amenity '''
-    #     rv = self.app.get('/amenities/500')
-    #     self.assertEqual(rv.status_code, 404)
-    #
-    # def test_delete(self):
-    #     ''' create an amenity and after delete it '''
-    #     rv = self.app.post('/amenities', headers={'Content-Type': 'application/json'}, data=json.dumps(good_amenity_1))
-    #     rv = self.app.get('/amenities')
-    #     before = len(json.loads(self.app.get('/amenities').data)['result'])
-    #     rv = self.app.delete('/amenities/1')
-    #     rv = self.app.get('/amenities')
-    #     after = len(json.loads(self.app.get('/amenities').data)['result'])
-    #
-    #     ''' check the status code '''
-    #     self.assertEqual(rv.status_code, 200)
-    #
-    #     ''' check the number of element before and after a delete '''
-    #     self.assertEqual(after - before, -1)
-    #
-    # def test_create_place_amenity(self):
-    #     ''' Set base data '''
-    #     rv = self.app.post('/states', headers={'Content-Type': 'application/json'}, data=json.dumps(good_state_1))
-    #     self.assertEqual(rv.status_code, 201)
-    #     rv = self.app.post('/states/1/cities', headers={'Content-Type': 'application/json'}, data=json.dumps(good_city_1))
-    #     self.assertEqual(rv.status_code, 201)
-    #     rv = self.app.post('/users', headers={'Content-Type': 'application/json'}, data=json.dumps(good_user_1))
-    #     self.assertEqual(rv.status_code, 201)
-    #     rv = self.app.post('/places', headers={'Content-Type': 'application/json'}, data=json.dumps(good_place_1))
-    #     self.assertEqual(rv.status_code, 201)
-    #     rv = self.app.post('/amenities', headers={'Content-Type': 'application/json'}, data=json.dumps(good_amenity_1))
-    #     self.assertEqual(rv.status_code, 201)
-    #
-    #     ''' Test if place does not exist '''
-    #     rv = self.app.post('/places/404/amenities/1')
-    #     self.assertEqual(rv.status_code, 404)
-    #
-    #     ''' Test if amenity does not exist '''
-    #     rv = self.app.post('/places/1/amenities/404')
-    #     self.assertEqual(rv.status_code, 404)
-    #
-    #     ''' Create new amenity for place '''
-    #     rv = self.app.post('/places/1/amenities/1')
-    #     self.assertEqual(rv.status_code, 201)
-    #
-    #     ''' Test if place, amenity combo exists '''
-    #     rv = self.app.post('/places/1/amenities/1')
-    #     self.assertEqual(rv.status_code, 400)
+    def test_create(self):
+        ''' Test creation of new amenities '''
+        rv = self.app.post('/amenities', headers={'Content-Type': 'application/json'}, data=json.dumps(good_amenity_1))
+        self.assertEqual(rv.status_code, 201)
+        data = json.loads(rv.data)
+        self.assertEqual(data['id'], 1)
+
+        rv = self.app.post('/amenities', headers={'Content-Type': 'application/json'}, data=json.dumps(good_amenity_2))
+        self.assertEqual(rv.status_code, 201)
+        data = json.loads(rv.data)
+        self.assertEqual(data['id'], 2)
+
+        ''' Test if name is NULL '''
+        rv = self.app.post('/amenities', headers={'Content-Type': 'application/json'}, data=json.dumps(bad_amenity_1))
+        self.assertEqual(rv.status_code, 400)
+
+        ''' Test if name is not string type '''
+        rv = self.app.post('/amenities', headers={'Content-Type': 'application/json'}, data=json.dumps(bad_amenity_2))
+        self.assertEqual(rv.status_code, 400)
+
+        ''' Test if key 'name' is missing '''
+        rv = self.app.post('/amenities', headers={'Content-Type': 'application/json'}, data=json.dumps(bad_amenity_3))
+        self.assertEqual(rv.status_code, 400)
+
+    def test_list(self):
+        ''' Check if no amenities '''
+        rv = self.app.get('/amenities')
+        self.assertEqual(rv.status_code, 200)
+        data = json.loads(rv.data)['result']
+        self.assertEqual(len(data), 0)
+
+        ''' Create new amenity '''
+        rv = self.app.post('/amenities', headers={'Content-Type': 'application/json'}, data=json.dumps(good_amenity_1))
+        self.assertEqual(rv.status_code, 201)
+
+        ''' Check if one amenity '''
+        rv = self.app.get('/amenities')
+        self.assertEqual(rv.status_code, 200)
+        data = json.loads(rv.data)['result']
+        self.assertEqual(len(data), 1)
+
+    def test_get(self):
+        ''' Set base data '''
+        rv = self.app.post('/amenities', headers={'Content-Type': 'application/json'}, data=json.dumps(good_amenity_1))
+        self.assertEqual(rv.status_code, 201)
+        data = json.loads(rv.data)
+        self.assertEqual(data['id'], 1)
+
+        ''' Retrieve the newly created amenity '''
+        rv = self.app.get('/amenities/1')
+        self.assertEqual(rv.status_code, 200)
+
+        ''' Test if amenity is the created amenity '''
+        amenity = json.loads(rv.data)
+        self.assertEqual(amenity['name'], good_amenity_1['name'])
+
+        ''' Test if amenity does not exist '''
+        rv = self.app.get('/amenities/404')
+        self.assertEqual(rv.status_code, 404)
+
+    def test_delete(self):
+        ''' Set base data '''
+        rv = self.app.post('/amenities', headers={'Content-Type': 'application/json'}, data=json.dumps(good_amenity_1))
+        self.assertEqual(rv.status_code, 201)
+
+        ''' Test if amenity does not exist '''
+        rv = self.app.delete('/amenities/404')
+        self.assertEqual(rv.status_code, 404)
+
+        ''' Test number of amenities before delete '''
+        rv = self.app.get('/amenities')
+        data = json.loads(rv.data)['result']
+        self.assertEqual(len(data), 1)
+
+        ''' Delete the existing amenity '''
+        rv = self.app.delete('/amenities/1')
+        self.assertEqual(rv.status_code, 200)
+
+        ''' Test number of amenities after delete '''
+        rv = self.app.get('/amenities')
+        data = json.loads(rv.data)['result']
+        self.assertEqual(len(data), 0)
+
+    def test_create_place_amenity(self):
+        ''' Set base data '''
+        rv = self.app.post('/states', headers={'Content-Type': 'application/json'}, data=json.dumps(good_state_1))
+        self.assertEqual(rv.status_code, 201)
+        rv = self.app.post('/states/1/cities', headers={'Content-Type': 'application/json'}, data=json.dumps(good_city_1))
+        self.assertEqual(rv.status_code, 201)
+        rv = self.app.post('/users', headers={'Content-Type': 'application/json'}, data=json.dumps(good_user_1))
+        self.assertEqual(rv.status_code, 201)
+        rv = self.app.post('/places', headers={'Content-Type': 'application/json'}, data=json.dumps(good_place_1))
+        self.assertEqual(rv.status_code, 201)
+        rv = self.app.post('/amenities', headers={'Content-Type': 'application/json'}, data=json.dumps(good_amenity_1))
+        self.assertEqual(rv.status_code, 201)
+
+        ''' Test if place does not exist '''
+        rv = self.app.post('/places/404/amenities/1')
+        self.assertEqual(rv.status_code, 404)
+
+        ''' Test if amenity does not exist '''
+        rv = self.app.post('/places/1/amenities/404')
+        self.assertEqual(rv.status_code, 404)
+
+        ''' Create new amenity for place '''
+        rv = self.app.post('/places/1/amenities/1')
+        self.assertEqual(rv.status_code, 201)
+
+        ''' Test error if place, amenity combo exists '''
+        rv = self.app.post('/places/1/amenities/1')
+        self.assertEqual(rv.status_code, 400)
 
     def test_get_place_amenities(self):
         ''' Set base data '''
