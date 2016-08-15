d8322064aadb5c717a03517f6c9fd7b0c9ef1c30
rickharris-dev <rickharris724@gmail.com>
2016-08-15 13:00:06 -0700

    Testing with Gunicorn

---
+++ api/wsgi.py
@@ -0,0 +1,6 @@
+from app import app
+
+'''this code will run only if this file run directly not from another file'''
+if __name__ == '__main__' :
+    ''' Initializes the flask api server '''
+    app.run()
