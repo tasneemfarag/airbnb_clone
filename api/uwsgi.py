333b73cecf005e0fa20a5442ef68e9a813fafaf1
rickharris-dev <rickharris724@gmail.com>
2016-08-14 21:53:13 -0700

    Updated int to str for concat

--- api/uwsgi.py
+++ api/uwsgi.py
@@ -3,4 +3,4 @@
 from subprocess import call
 from config import *
 
-call(["uwsgi", "--socket", "127.0.0.1:" + PORT, "--wsgi-file", "app.py", "--callable", "app", "--processes", "4", "--threads", "2", "--stats", "127.0.0.1:9191"])
+call(["uwsgi", "--socket", "127.0.0.1:" + str(PORT), "--wsgi-file", "app.py", "--callable", "app", "--processes", "4", "--threads", "2", "--stats", "127.0.0.1:9191"])
